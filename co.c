de100439addac6207af70b5967b81df060174d30
mk master <79350888+MKmasterg@users.noreply.github.com>
2025-05-27 14:06:35 +0330

    Implement co_init

--- co.c
+++ co.c
@@ -4,17 +4,62 @@
 #include <unistd.h>
 #include <stdatomic.h>
 #include <signal.h>
-#ifdef __linux__
-#include <sys/prctl.h>
-#include <bits/types/sigset_t.h>
-#include <bits/sigaction.h>
-#else
-// macOS includes
-#include <signal.h>
-#endif
+#include <pthread.h>
+
+typedef struct {
+    task_t arr[TASK_QUEUE_SIZE];
+    int front;
+    int rear;
+    int size;
+    pthread_mutex_t lock;
+    pthread_cond_t not_empty;
+} Queue;
+
+static Queue *task_queue_global = NULL;
+
+void *worker_thread_func(void *arg) {
+    Queue *task_queue = (Queue *)arg;
+
+    pthread_mutex_lock(&task_queue->lock);
+    while (true) {
+        // Wait for a task to be available
+        while (task_queue->size == 0) {
+            pthread_cond_wait(&task_queue->not_empty, &task_queue->lock);
+        }
+
+        pthread_mutex_lock(&task_queue->lock);
+        if (task_queue->size == 0) {
+            pthread_mutex_unlock(&task_queue->lock);
+            continue;  // No task available, continue waiting
+        }
+        task_t task = task_queue->arr[task_queue->front];
+        task_queue->front = (task_queue->front + 1) % TASK_QUEUE_SIZE;
+        task_queue->size--;
+        pthread_mutex_unlock(&task_queue->lock);
+
+        // Execute the task
+        if (task.func != NULL) {
+            task.func(task.arg);
+        }
+    }
+    return NULL;
+}
 
 void co_init() {
-    // TO BE IMPLEMENTED
+    // Initializes a task queue with TASK_QUEUE_SIZE capacity
+    task_queue_global = malloc(sizeof(Queue));
+    task_queue_global->front = 0;
+    task_queue_global->rear = 0;
+    task_queue_global->size = 0;
+    
+    pthread_mutex_init(&task_queue_global->lock, NULL);
+    pthread_cond_init(&task_queue_global->not_empty, NULL);
+
+    // Creates a thread pool with WORKER_COUNT threads
+    for (int i = 0; i < WORKER_COUNT; i++) {
+        pthread_t worker_thread;
+        pthread_create(&worker_thread, NULL, worker_thread_func, task_queue_global);
+    }
 }
 
 void co_shutdown() {
@@ -23,6 +68,7 @@ void co_shutdown() {
 
 void co(task_func_t func, void *arg) {
     // TO BE IMPLEMENTED
+    // 
 }
 
 int wait_sig() {
