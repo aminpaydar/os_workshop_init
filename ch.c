90665825cdb6de2a60d620f10808b60e1de69e6b
mk master <79350888+MKmasterg@users.noreply.github.com>
2025-05-27 15:00:21 +0330

    Implement channel creation and send mechanism

--- ch.c
+++ ch.c
@@ -6,15 +6,30 @@
 #include <stdatomic.h>
 
 channel_t *make_ch() {
-    // TO BE IMPLEMENTED
-    return NULL;
+    channel_t *ch = malloc(sizeof(channel_t));
+    ch->head = 0;
+    ch->tail = 0;
+    ch->size = 0;
+
+    pthread_mutex_init(&ch->mutex, NULL);
+    pthread_cond_init(&ch->cond_send, NULL);
+    pthread_cond_init(&ch->cond_recv, NULL);
+
+    return ch;
 }
 
 void channel_send(channel_t *ch, void *data) {
-    // TO BE IMPLEMENTED
+    pthread_mutex_lock(&ch->mutex);
+    while (ch->size == 0) {
+        pthread_cond_wait(&ch->cond_send, &ch->mutex);
+    }
+    ch->buffer[ch->tail] = data;
+    ch->tail = (ch->tail + 1) % CHANNEL_CAPACITY;
+    ch->size++;
+    pthread_cond_signal(&ch->cond_recv);
+    pthread_mutex_unlock(&ch->mutex);
 }
 
 void *channel_recv(channel_t *ch) {
-   // TO BE IMPLEMENTED
-   return NULL;
+
 }
