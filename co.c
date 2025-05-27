71f65c7bef35c289c43fe311c1ba830d2472ed61
Mohammadjavad <mohamadjavad.deh1384@gmail.com>
2025-05-27 15:39:25 +0330

    change_co.c

--- co.c
+++ co.c
@@ -6,8 +6,6 @@
 #include <signal.h>
 #ifdef __linux__
 #include <sys/prctl.h>
-#include <bits/types/sigset_t.h>
-#include <bits/sigaction.h>
 #else
 // macOS includes
 #include <signal.h>
@@ -20,10 +18,16 @@ typedef struct {
     pthread_cond_t cond;
 } task_queue_t;
 
-task_queue_t task_queue = {.head = 0, .tail = 0, .mutex = PTHREAD_MUTEX_INITIALIZER, .cond = PTHREAD_COND_INITIALIZER};
-pthread_t workers[WORKER_COUNT];
-atomic_bool running = true;
-atomic_bool workers_init = false;
+static task_queue_t task_queue = {
+    .head = 0,
+    .tail = 0,
+    .mutex = PTHREAD_MUTEX_INITIALIZER,
+    .cond = PTHREAD_COND_INITIALIZER
+};
+
+static pthread_t workers[WORKER_COUNT];
+static atomic_bool running = true;
+static atomic_bool workers_init = false;
 
 void task_queue_push(task_func_t func, void *arg) {
     pthread_mutex_lock(&task_queue.mutex);
@@ -47,9 +51,12 @@ task_t task_queue_pop() {
 
 void *worker_thread(void *arg) {
     int thread_id = *(int*) arg;
+
+#ifdef __linux__
     char thread_name[32];
-    sprintf(thread_name, "Worker-%d", thread_id);
+    snprintf(thread_name, sizeof(thread_name), "Worker-%d", thread_id);
     prctl(PR_SET_NAME, thread_name, 0, 0, 0);
+#endif
 
     while (running) {
         task_t task = task_queue_pop();
@@ -64,14 +71,11 @@ void *worker_thread(void *arg) {
 
 void co_init() {
     if (workers_init) return;
-    int worker_ids[WORKER_COUNT];
-    for (int i = 0; i < WORKER_COUNT; i++) {
-        worker_ids[i] = i + 1;
-    }
+
     for (int i = 0; i < WORKER_COUNT; i++) {
         int *thread_id = malloc(sizeof(int));
         *thread_id = i + 1;
-        pthread_create(&workers[i], NULL, worker_thread, (void*) thread_id);
+        pthread_create(&workers[i], NULL, worker_thread, thread_id);
     }
 
     workers_init = true;
@@ -80,6 +84,7 @@ void co_init() {
 void co_shutdown() {
     running = false;
     pthread_cond_broadcast(&task_queue.cond);
+
     for (int i = 0; i < WORKER_COUNT; i++) {
         pthread_join(workers[i], NULL);
     }
@@ -89,16 +94,18 @@ void co(task_func_t func, void *arg) {
     task_queue_push(func, arg);
 }
 
-
 int wait_sig() {
     sigset_t mask;
     sigemptyset(&mask);
     sigaddset(&mask, SIGINT);
     sigaddset(&mask, SIGTERM);
-    pthread_sigmask(SIG_BLOCK, &mask, NULL);  // Block signals so they are handled by sigwait
+    pthread_sigmask(SIG_BLOCK, &mask, NULL);
+
     printf("Waiting for SIGINT (Ctrl+C) or SIGTERM...\n");
+
     int signum;
-    sigwait(&mask, &signum);  // Wait for a signal
+    sigwait(&mask, &signum);
+
     printf("Received signal %d, shutting down...\n", signum);
     return signum;
 }
