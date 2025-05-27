1155a1130d3f35b96e63bbd32b58ad7b52e54be3
mh-nemati <mohnemati84@gmail.com>
2025-05-27 16:24:16 +0330

    MohammadHossein Nemati

--- ch.c
+++ ch.c
@@ -5,16 +5,59 @@
 #include <unistd.h>
 #include <stdatomic.h>
 
+#include "ch.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 channel_t *make_ch() {
-    // TO BE IMPLEMENTED
-    return NULL;
+    channel_t *ch = malloc(sizeof(channel_t));
+    if (&ch == NULL) 
+        return NULL;
+
+    ch->head = 0;
+    ch->tail = 0;
+    ch->size = 0;
+
+    pthread_mutex_init(&ch->mutex, NULL);
+    pthread_cond_init(&ch->cond_send, NULL);
+    pthread_cond_init(&ch->cond_recv, NULL);
+
+    return ch;
 }
 
 void channel_send(channel_t *ch, void *data) {
-    // TO BE IMPLEMENTED
+    pthread_mutex_lock(&ch->mutex);
+
+    while (ch->size == CHANNEL_CAPACITY) {
+        pthread_cond_wait(&ch->cond_send, &ch->mutex);
+    }
+
+    // Add data to buffer
+    ch->buffer[ch->tail] = data;
+    ch->tail = (ch->tail + 1) % CHANNEL_CAPACITY;
+    ch->size++;
+
+    // Signal receiver
+    pthread_cond_signal(&ch->cond_recv);
+    pthread_mutex_unlock(&ch->mutex);
 }
 
 void *channel_recv(channel_t *ch) {
-   // TO BE IMPLEMENTED
-   return NULL;
+    pthread_mutex_lock(&ch->mutex);
+
+    // Wait if buffer is empty
+    while (ch->size == 0) {
+        pthread_cond_wait(&ch->cond_recv, &ch->mutex);
+    }
+
+    // Get data from buffer
+    void *data = ch->buffer[ch->head];
+    ch->head = (ch->head + 1) % CHANNEL_CAPACITY;
+    ch->size--;
+
+    // Signal sender
+    pthread_cond_signal(&ch->cond_send);
+    pthread_mutex_unlock(&ch->mutex);
+
+    return data;
 }
