ac25c31fd19d1a771734a90d72f53d4a25f8716e
Mehdi <meh.vares@gmail.com>
2025-05-27 16:13:54 +0330

    mehdi vareth

--- ch.c
+++ ch.c
@@ -7,14 +7,42 @@
 
 channel_t *make_ch() {
     // TO BE IMPLEMENTED
-    return NULL;
+    channel_t ch = {
+        .buffer = (void *) malloc(CHANNEL_CAPACITY),
+        .cond_recv = PTHREAD_COND_INITIALIZER,
+        .cond_send = PTHREAD_COND_INITIALIZER,
+        .head = 0, .tail = 0,
+        .mutex = PTHREAD_MUTEX_INITIALIZER,
+        .size = CHANNEL_CAPACITY
+    };
+    
+    return &ch;
 }
 
 void channel_send(channel_t *ch, void *data) {
-    // TO BE IMPLEMENTED
+    
+    pthread_mutex_lock(&ch->mutex);
+    while (ch->size == CHANNEL_CAPACITY)
+        pthread_cond_wait(&ch->cond_send, &ch->mutex);
+    
+    ch->buffer[ch->tail] = data;
+    ch->tail = (ch->tail + 1) % CHANNEL_CAPACITY;
+    ch->size ++;
+    pthread_cond_signal(&ch->cond_recv);
+    pthread_mutex_unlock(&ch->mutex);
 }
 
 void *channel_recv(channel_t *ch) {
-   // TO BE IMPLEMENTED
-   return NULL;
+   
+    pthread_mutex_lock(&ch->mutex);
+    while (ch->size == 0)
+        pthread_cond_wait(&ch->cond_recv, &ch->mutex);
+    
+    void *data = ch->buffer[ch->head];
+    ch->head = (ch->head + 1) % CHANNEL_CAPACITY;
+    ch->size --;
+    pthread_cond_signal(&ch->cond_send);
+    pthread_mutex_unlock(&ch->mutex);
+
+   return data;
 }
