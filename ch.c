b5700e2ba0ce40e8ccdc3a72bb359081b0bc81cb
Mohammad hadi Niknam <niknam@aut.ac.ir>
2025-05-27 19:28:00 +0330

    ch handeled , We handle channle globally which make it not initialize correctly

--- ch.c
+++ ch.c
@@ -5,16 +5,56 @@
 #include <unistd.h>
 #include <stdatomic.h>
 
+channel_t ch ;
+
 channel_t *make_ch() {
-    // TO BE IMPLEMENTED
-    return NULL;
+    pthread_mutex_init(&ch.mutex, NULL);
+    pthread_cond_init(&ch.cond_send, NULL);
+    pthread_cond_init(&ch.cond_recv, NULL);
+    ch.head = 0;
+    ch.tail = CHANNEL_CAPACITY;
+    //ch.tail = 0 ; 
+    ch.size = 0 ;
+    for(int i = 0 ; i < CHANNEL_CAPACITY ; i++){
+        //*(ch.buffer + i) = NULL ;
+        ch.buffer[i] = NULL;
+    }
+
+    return &ch;
 }
 
+// برای کانسیومر باید یه ویت باشه که وقتی چیزی 
+// اومد این متوجه بشه و یکی از اون پرودیوسر ها گذاشت یه سیگنال بده
 void channel_send(channel_t *ch, void *data) {
-    // TO BE IMPLEMENTED
+    pthread_mutex_lock(&ch->mutex);
+    while(ch->size == CHANNEL_CAPACITY){
+        pthread_cond_wait(&ch->cond_recv, &ch->mutex);
+    }
+    //printf("%d",ch->tail);
+    ch->buffer[ch->tail] = data;
+    ch->tail = (ch->tail + 1) % CHANNEL_CAPACITY;
+    ch->size++;
+    pthread_cond_signal(&ch->cond_send);
+    pthread_mutex_unlock(&ch->mutex);
 }
 
 void *channel_recv(channel_t *ch) {
-   // TO BE IMPLEMENTED
-   return NULL;
+    pthread_mutex_lock(&ch->mutex);
+    while(ch->size == 0){
+        pthread_cond_wait(&ch->cond_send, &ch->mutex);
+    }
+    /*
+    
+    while(ch->head == ch->tail){
+        pthread_cond_wait(&ch.cond_recv , &ch.mutex );
+    }
+
+    */
+    printf("%d",ch->head);
+    void *data = ch->buffer[ch->head];
+    ch->head = (ch->head + 1) % CHANNEL_CAPACITY;
+    ch->size--;
+    pthread_cond_signal(&ch->cond_recv);
+    pthread_mutex_unlock(&ch->mutex);
+    return data;
 }
--- co.c
+++ co.c
@@ -13,16 +13,105 @@
 #include <signal.h>
 #endif
 
-void co_init() {
-    // TO BE IMPLEMENTED
-}
+pthread_mutex_t lock1;
+
+typedef struct {
+    task_t Q[TASK_QUEUE_SIZE];
+    int head;
+    int tail;
+    pthread_mutex_t lock;
+    pthread_cond_t cond;
+} Queue;
+
+typedef struct ThreadPool {
+    int head;
+    int tail;
+    pthread_t threadpool[WORKER_COUNT];
+    pthread_mutex_t lock1;
+    pthread_cond_t cond1;
+} Pool;
+
+Queue func_queue;
+Pool TPool;
+
+atomic_bool running = ATOMIC_VAR_INIT(true);
+pthread_t workers[WORKER_COUNT];  // Added workers array declaration
 
 void co_shutdown() {
-    // TO BE IMPLEMENTED
+    running = false;
+    pthread_cond_broadcast(&func_queue.cond);
+    for (int i = 0; i < WORKER_COUNT; i++) {
+        pthread_join(workers[i], NULL);
+    }
+}
+
+void task_queue_push(task_func_t func, void *arg) {
+    pthread_mutex_lock(&func_queue.lock);
+    task_t task = {func, arg};
+    func_queue.Q[func_queue.tail] = task;
+    func_queue.tail = (func_queue.tail + 1) % TASK_QUEUE_SIZE;
+    pthread_cond_signal(&func_queue.cond);
+    pthread_mutex_unlock(&func_queue.lock);
+}
+
+task_t task_queue_pop() {
+    pthread_mutex_lock(&func_queue.lock);
+    while (func_queue.head == func_queue.tail && running) {
+        pthread_cond_wait(&func_queue.cond, &func_queue.lock);
+    }
+    task_t task = {0};
+    if (running) {
+        task = func_queue.Q[func_queue.head];
+        func_queue.head = (func_queue.head + 1) % TASK_QUEUE_SIZE;
+    }
+    pthread_mutex_unlock(&func_queue.lock);
+    return task;
+}
+
+void *worker_thread(void *arg) {
+    int thread_id = *(int*)arg;
+    char thread_name[32];
+#ifdef __linux__
+    sprintf(thread_name, "Worker-%d", thread_id);
+    prctl(PR_SET_NAME, thread_name, 0, 0, 0);
+#endif
+
+    while (running) {
+        task_t task = task_queue_pop();
+        if (task.func) {
+            task.func(task.arg);
+        }
+    }
+
+    free(arg);
+    return NULL;
+}
+
+void co_init() {
+
+    pthread_mutex_init(&func_queue.lock, NULL);
+    pthread_cond_init(&func_queue.cond, NULL);
+    func_queue.head = 0;
+    func_queue.tail = 0;
+    for (int i = 0; i < TASK_QUEUE_SIZE; i++) {
+        func_queue.Q[i].func = NULL;
+        func_queue.Q[i].arg = NULL;
+    }
+
+    TPool.head = 0;
+    TPool.tail = 0;
+    pthread_mutex_init(&TPool.lock1, NULL);
+    pthread_cond_init(&TPool.cond1, NULL);
+
+    for (int i = 0; i < WORKER_COUNT; i++) {
+        int *thread_id = malloc(sizeof(int));
+        *thread_id = i + 1;
+        pthread_create(&workers[i], NULL, worker_thread, (void *)thread_id);
+    }
 }
 
 void co(task_func_t func, void *arg) {
-    // TO BE IMPLEMENTED
+    task_queue_push(func, arg);
 }
 
 int wait_sig() {
