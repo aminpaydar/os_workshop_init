ffca4d9fae4aee9798b01db98d8af588e1e5de75
iamhamidhosseini <153517735+iamhamidhosseini@users.noreply.github.com>
2025-05-27 14:33:53 +0330

    co

--- co.c
+++ co.c
@@ -1,39 +1,82 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include "co.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdatomic.h>
+#include <pthread.h>
 #include <signal.h>
-#ifdef __linux__
-#include <sys/prctl.h>
-#include <bits/types/sigset_t.h>
-#include <bits/sigaction.h>
-#else
-// macOS includes
-#include <signal.h>
-#endif
 
-void co_init() {
-    // TO BE IMPLEMENTED
+#define MAX_COROUTINES 128
+
+typedef struct {
+    coroutine co;
+    atomic_int active;
+} co_entry;
+
+static co_entry coroutines[MAX_COROUTINES];
+static __thread coroutine *current_co = NULL;
+
+int co_create(coroutine *co, coroutine_func fn, void *arg) {
+    if (getcontext(&co->ctx) == -1) return -1;
+    co->stack = malloc(STACK_SIZE);
+    if (!co->stack) return -1;
+    co->ctx.uc_stack.ss_sp   = co->stack;
+    co->ctx.uc_stack.ss_size = STACK_SIZE;
+    co->ctx.uc_link          = &co->caller;
+    makecontext(&co->ctx, (void (*)(void))fn, 1, arg);
+    return 0;
 }
 
-void co_shutdown() {
-    // TO BE IMPLEMENTED
+void co_resume(coroutine *co) {
+    current_co = co;
+    swapcontext(&co->caller, &co->ctx);
+}
+
+void co_yield(void) {
+    swapcontext(&current_co->ctx, &current_co->caller);
+}
+
+void co_init(void) {
+    for (int i = 0; i < MAX_COROUTINES; i++) {
+        atomic_store(&coroutines[i].active, 0);
+        coroutines[i].co.stack = NULL;
+    }
+}
+
+void co_shutdown(void) {
+    for (int i = 0; i < MAX_COROUTINES; i++) {
+        if (atomic_load(&coroutines[i].active)) {
+            free(coroutines[i].co.stack);
+            atomic_store(&coroutines[i].active, 0);
+            coroutines[i].co.stack = NULL;
+        }
+    }
 }
 
 void co(task_func_t func, void *arg) {
-    // TO BE IMPLEMENTED
+    for (int i = 0; i < MAX_COROUTINES; i++) {
+        if (!atomic_load(&coroutines[i].active)) {
+            coroutine *c = &coroutines[i].co;
+            if (co_create(c, (coroutine_func)func, arg) == 0) {
+                atomic_store(&coroutines[i].active, 1);
+                co_resume(c);
+            }
+            return;
+        }
+    }
+    fprintf(stderr, "No available coroutine slots\n");
 }
 
-int wait_sig() {
+int wait_sig(void) {
     sigset_t mask;
     sigemptyset(&mask);
     sigaddset(&mask, SIGINT);
     sigaddset(&mask, SIGTERM);
-    pthread_sigmask(SIG_BLOCK, &mask, NULL);  // Block signals so they are handled by sigwait
+    sigprocmask(SIG_BLOCK, &mask, NULL);
     printf("Waiting for SIGINT (Ctrl+C) or SIGTERM...\n");
     int signum;
-    sigwait(&mask, &signum);  // Wait for a signal
+    sigwait(&mask, &signum);
     printf("Received signal %d, shutting down...\n", signum);
     return signum;
 }
--- co.h
+++ co.h
@@ -1,23 +1,25 @@
 #ifndef CO_H
 #define CO_H
 
-#include <pthread.h>
-#include <stdbool.h>
+#include <ucontext.h>
 
-#define WORKER_COUNT 32
-#define TASK_QUEUE_SIZE 100
+#define STACK_SIZE (64*1024)
 
 typedef void (*task_func_t)(void *arg);
+typedef void (*coroutine_func)(void *arg);
 
 typedef struct {
-    task_func_t func;
-    void *arg;
-} task_t;
+    ucontext_t ctx;
+    ucontext_t caller;
+    void *stack;
+} coroutine;
 
-void co_init();
-void co_shutdown();
+void co_init(void);
+void co_shutdown(void);
 void co(task_func_t func, void *arg);
+int co_create(coroutine *co, coroutine_func fn, void *arg);
+void co_resume(coroutine *co);
+void co_yield(void);
+int wait_sig(void);
 
-int wait_sig();
-
-#endif // CO_H
+#endif
